c9402b795d3321605259fca5655b3c22920b9155
Natali <nana_projetti@hotmail.com>
2024-09-25 20:36:49 -0300

    Atualização

--- products/delete.php
+++ products/delete.php
@@ -3,14 +3,14 @@
 require_once("../controller/controllerProducts.php");
 require_once("../model/modelProducts.php");
 
-if($_SERVER"[REQUEST_METHOD]" == "DELETE") {
+if($_SERVER["REQUEST_METHOD"] == "DELETE") {
     
     $id = $_GET["id"];
-    $controllerProduct = new controllerProduct();
-    $delete = $controllerProduct->delete($id);
+    $controllerProducts = new controllerProducts();
+    $delete = $controllerProducts->delete($id);
 
     if($delete) {
-        $msg = array("msg" => "Product does not deleted.");
+        $msg = array("msg" => "Product has been deleted successfully.");
         echo json_encode($msg);
     } else {
         $msg = array("msg" => "Error, Product does not deleted.");
