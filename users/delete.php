48ff2006bba99bce21927eb092bd732b847aaac9
Natali <nana_projetti@hotmail.com>
2024-10-14 19:57:25 -0300

    Atualização dos usuários e da model

---
+++ users/delete.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once("../controller/controllerUsers.php");
+require_once("../model/modelUsers.php");
+
+if($_SERVER["REQUEST_METHOD"] == "DELETE") {
+
+    $query = $_SERVER["QUERY_STRING"];
+    parse_str($query, $params);
+    $id = $params["id"];
+
+    $controllerUsers = new controllerUsers();
+    $delete = $controllerUsers->delete($id);
+
+    if($delete) {
+        $msg = array("msg" => "Delete was deleted successfully.");
+        echo json_encode($msg);
+    } else {
+        $msg = array("msg" => "Error, delete was not deleted.");
+        echo json_encode($msg);
+    }
+
+} else {
+    header("HTTP/1.1 405 Method Not Allowed");
+}
